e6c08c51aff8047ead726c63996d077222370bde
leeheejuuun <94230809+leeheejuuun@users.noreply.github.com>
2022-04-20 21:57:19 +0900

    useEffect 오류 수정

--- src/pages/heejun/Login/Login.js
+++ src/pages/heejun/Login/Login.js
@@ -1,5 +1,5 @@
 import './Login.scss';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
